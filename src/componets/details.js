232bafb8aa3b3b5cf6400d791361bb28f4bd3093
Chuanqi Zhu <chuanqizhu1991@outlook.com>
2021-11-30 12:59:43 +0000

    Fetch data in the detail page

--- src/componets/details.js
+++ src/componets/details.js
@@ -1,6 +1,39 @@
-function Details(props){
+
+import { useState, useEffect } from 'react';
+
+function Details({country}){
+
+   let datenow = new Date();
+   let datebefore = new Date();
+   datenow.setDate(datenow.getDate() - 1);
+   datenow = datenow.toISOString().split('T')[0];
+   datebefore.setDate(datebefore.getDate() - 7);
+   datebefore = datebefore.toISOString().split('T')[0];
+   const [data, setData] = useState(null);
+
+//    /spain?date_from=2020-03-20&date_to=2020-03-22
+   useEffect(()=>fetch(`https://api.covid19tracking.narrativa.com/api/country/${country}?date_from=${datebefore}&date_to=${datenow}`)
+  .then(res => res.json())
+  .then(data => setData(data.dates)),[])// eslint-disable-line
+
+  console.log(data);
+//   console.log(Object.values(data["2021-11-26"])[0]);
+
     return (
-        <h1>here are the details of {props.country}</h1>
+        <div>
+             <h1>here are the details of {country}</h1>
+             {!!data ? Object.keys(data).map((item)=>{
+                 return (
+                     <>
+                     <div>{item}</div>
+                     <div>{Object.values(Object.values(data[item])[0])[0].today_new_confirmed}</div>
+                     <div>{Object.values(Object.values(data[item])[0])[0].today_new_deaths}</div>
+                     </>
+                 )
+             }
+             ): ""}
+
+        </div>
     )
 }
 
--- src/componets/home.js
+++ src/componets/home.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react';
+import './home.css';
+
 export const countryList = ['China', 'Japan', 'Germany', 'US', "United Kingdom", "Russia", "France"];
 
 function Home(){
@@ -6,30 +8,27 @@ function Home(){
    let datenow = new Date();
    datenow.setDate(datenow.getDate() - 1);
    datenow = datenow.toISOString().split('T')[0];
-   console.log(datenow);
    const [data, setData] = useState(null);
 
    useEffect(()=>fetch(`https://api.covid19tracking.narrativa.com/api/${datenow}`)
   .then(res => res.json())
-  .then(data => setData(data)), [])
-
-  console.log(data);
+  .then(data => setData(data)),[])// eslint-disable-line
 
     return (
         <>
         <h4>{datenow}</h4>
-        <ul>
+        <div className="row row-cols-2">
             {countryList.map((item)=>
             {
                 return (<a href={`details/${item.toLowerCase()}`}>
-                <li key={item}>
+                <div key={item} className="home-items col d-flex flex-row justify-content-between">
                     <div>{item}</div>
                     <div>{!!data ? data.dates[datenow].countries[item].today_new_confirmed : "0"}</div>
-                </li>
+                </div>
                 </a>)
             }
                 )}
-        </ul>
+        </div>
         
         </>
     )
